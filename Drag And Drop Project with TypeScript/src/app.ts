1c8f524dd07141419bb8883847a6e4ff01d06fbc
NatnaelH <natnaelhabtamu0@gmail.com>
2022-06-07 16:47:37 +0300

    Interacting with DOM elements

--- Drag And Drop Project with TypeScript/src/app.ts
+++ Drag And Drop Project with TypeScript/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput{
     templateElement : HTMLTemplateElement;
     hostElement : HTMLDivElement;
     element : HTMLFormElement;
+    titleInput : HTMLInputElement;
+    descriptionInput: HTMLInputElement;
+    peopleInput : HTMLInputElement;
 
     constructor(){
         this.templateElement = document.getElementById("project-input") ! as HTMLTemplateElement;
@@ -9,12 +12,26 @@ class ProjectInput{
         const importedNode = document.importNode(this.templateElement.content, true)
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id = "user-input"
+        this.titleInput = this.element.querySelector("#title") ! as HTMLInputElement;
+        this.descriptionInput  = this.element.querySelector("#description") ! as HTMLInputElement;
+        this.peopleInput = this.element.querySelector("#people") ! as HTMLInputElement;
+        this.configure()
         this.attach()
     }
 
     private attach(){
         this.hostElement.insertAdjacentElement("afterbegin", this.element)
+    }
+    private handleSubmit(event: Event){
+        event.preventDefault()
+        console.log(this.titleInput.value)
+        console.log(this.descriptionInput.value)
+        console.log(this.peopleInput.value)
+    }
 
+    private configure(){
+        this.element.addEventListener('submit', this.handleSubmit.bind(this))
+        
     }
 }
 
