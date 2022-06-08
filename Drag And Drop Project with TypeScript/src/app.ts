d8e158dbb4b212ae2ab9a5927ab692e4dda56a1a
NatnaelH <natnaelhabtamu0@gmail.com>
2022-06-08 16:34:24 +0300

    Added fetching user input and validation

--- Drag And Drop Project with TypeScript/src/app.ts
+++ Drag And Drop Project with TypeScript/src/app.ts
@@ -1,3 +1,49 @@
+//validatable interface
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+  }
+// validate function
+
+function Validate(validatableInput: Validatable) {
+    let isValid = true;
+    if (validatableInput.required) {
+      isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (
+      validatableInput.minLength != null &&
+      typeof validatableInput.value === 'string'
+    ) {
+      isValid =
+        isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (
+      validatableInput.maxLength != null &&
+      typeof validatableInput.value === 'string'
+    ) {
+      isValid =
+        isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (
+      validatableInput.min != null &&
+      typeof validatableInput.value === 'number'
+    ) {
+      isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (
+      validatableInput.max != null &&
+      typeof validatableInput.value === 'number'
+    ) {
+      isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+  }
+
+
 // autobind decorator
 function autobind(
     _: any,
@@ -41,12 +87,59 @@ class ProjectInput{
         this.hostElement.insertAdjacentElement("afterbegin", this.element)
     }
 
+    private gatherInputs():[string, string , number] | void{
+        const enteredTitle  = this.titleInput.value;
+        const enteredDescription = this.descriptionInput.value;
+        const enteredPeople = this.peopleInput.value;
+        // return [enteredTitle , enteredDesc , enteredPeople]
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true
+          };
+          const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+          };
+          const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+          };
+
+          if (
+            !Validate(titleValidatable) ||
+            !Validate(descriptionValidatable) ||
+            !Validate(peopleValidatable)
+          ) {
+            alert('Invalid input, please try again!');
+            return;
+          } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+          }
+        }
+
+    private clearInputs(){
+        this.titleInput.value = '';
+        this.descriptionInput.value = '';
+        this.peopleInput.value = '';
+    }
+
     @autobind
     private handleSubmit(event: Event){
         event.preventDefault()
-        console.log(this.titleInput.value)
-        console.log(this.descriptionInput.value)
-        console.log(this.peopleInput.value)
+        const userInput  = this.gatherInputs()
+        if(Array.isArray(userInput)){
+            const [title, desc , people] = userInput
+            console.log(title, desc, people)
+            this.clearInputs()
+        }
+        // console.log(userInput)
+        // console.log(this.titleInput.value)
+        // console.log(this.descriptionInput.value)
+        // console.log(this.peopleInput.value)
     }
 
     private configure(){
